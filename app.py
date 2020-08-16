2dba4fb4ed2e9f0fc4f267afc82630b728041d19
James Chu <jamesdchu@gmail.com>
2020-08-16 15:47:25 -0400

    Start

--- app.py
+++ app.py
@@ -1,46 +1,21 @@
-# ---- YOUR APP STARTS HERE ----
-# -- Import section --
-from flask import Flask
-from flask import render_template
-from flask import request
-# from flask_pymongo import PyMongo
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-
-# -- Initialization section --
 app = Flask(__name__)
-
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
-
-# name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
-
-# URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
-
-# mongo = PyMongo(app)
-
-# -- Routes section --
-# INDEX
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 @app.route('/')
-@app.route('/index')
+def sessions():
+    return render_template('session.html')
 
-def index():
-    return render_template('index.html', events = events)
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
 
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
-# CONNECT TO DB, ADD DATA
-
-@app.route('/add')
-
-def add():
-    # connect to the database
-
-    # insert new data
-
-    # return a message to the user
-    return ""
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
